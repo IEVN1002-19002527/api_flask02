42df278e50c607f7994493e97567a624331cceb0
Oscar Meza <meza03994@gmail.com>
2025-10-24 20:57:03 -0600

    Distancia

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import math
 
 app = Flask(__name__)
 
@@ -8,12 +9,30 @@ def index():
     lista = ["Python", "Flask", "HTML", "CSS", "JavaScript"]
     return render_template('index.html', titulo=titulo, lista=lista)
 
-@app.route('/calculos')
+@app.route('/calculos', methods=['GET', 'POST'])
 def calculos():
+    
+    if request.method == 'POST':
+        numero1 = request.form['numero1']
+        numero2 = request.form['numero2']
+        opcin = request.form['opcion']
+        if opcin == 'suma':
+            resultado = int(numero1) + int(numero2)
+        if opcin == 'resta':
+            resultado = int(numero1) - int(numero2)
+        return render_template('calculos.html', resultado=resultado, numero1=numero1, numero2=numero2)
     return render_template('calculos.html')
 
-@app.route('/distancia')
+@app.route('/distancia', methods=['GET', 'POST'])
 def distancia():
+
+    if request.method == 'POST':
+        num1 = float(request.form['numero1'])
+        num2 = float(request.form['numero2'])
+        num3 = float(request.form['numero3'])
+        num4 = float(request.form['numero4'])
+        distancia = math.sqrt(math.pow(num3 - num1, 2) + math.pow(num4 - num2, 2))
+        return render_template('distancia.html', distancia=distancia, num1=num1, num2=num2, num3=num3, num4=num4)
     return render_template('distancia.html')
 
 @app.route('/user/<string:user>')
